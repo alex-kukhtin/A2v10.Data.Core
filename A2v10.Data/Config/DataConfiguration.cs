d8e5f87ecc4c2b3772f5a0d2c13fa8b6af8cba72
Аlex Kukhtin <alex.kukhtin@outlook.com>
2020-06-23 09:38:58 +0300

    v 7241

---
+++ A2v10.Data/Config/DataConfiguration.cs
@@ -0,0 +1,29 @@
+﻿// Copyright © 2020 Alex Kukhtin. All rights reserved.
+
+using System;
+using Microsoft.Extensions.Configuration;
+using A2v10.Data.Interfaces;
+
+namespace A2v10.Data.Config
+{
+	public class DataConfiguration : IDataConfiguration
+	{
+		private readonly IConfiguration _config;
+		public DataConfiguration(IConfiguration config)
+		{
+			_config = config ?? throw new ArgumentNullException(nameof(config));
+		}
+
+		#region IDataConfiguration
+
+		public TimeSpan CommandTimeout => _config.GetValue<TimeSpan>("A2v10:Data:CommandTimeout");
+
+		public string ConnectionString(string source)
+		{
+			if (String.IsNullOrEmpty(source))
+				source = "DefaultConnection";
+			return _config.GetConnectionString(source);
+		}
+		#endregion
+	}
+}
---
+++ A2v10.Data/Extensions/Services.cs
@@ -0,0 +1,20 @@
+﻿// Copyright © 2020 Alex Kukhtin. All rights reserved.
+
+using Microsoft.Extensions.DependencyInjection;
+
+using A2v10.Data.Interfaces;
+using A2v10.Data.Config;
+
+namespace A2v10.Data.Extensions
+{
+	public static class Services
+	{
+		public static void UseSimpleDbContext(this IServiceCollection services) 
+		{
+			services.AddSingleton<IDataConfiguration, DataConfiguration>();
+			services.AddSingleton<IDataProfiler, NullDataProfiler>();
+			services.AddSingleton<IDataLocalizer, NullDataLocalizer>();
+			services.AddSingleton<IDbContext, SqlDbContext>();
+		}
+	}
+}
