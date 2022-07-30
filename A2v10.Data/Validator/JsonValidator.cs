160981e41f7182dfd0f283a4a46c80c997150096
alex-kukhtin <alex.kukhtin@outlook.com>
2022-07-30 15:57:25 +0300

    test commit from new pc

--- A2v10.Data/Validator/JsonValidator.cs
+++ A2v10.Data/Validator/JsonValidator.cs
@@ -1,38 +1,36 @@
-﻿// Copyright © 2015-2019 Alex Kukhtin. All rights reserved.
+﻿// Copyright © 2015-2022 Alex Kukhtin. All rights reserved.
 
-using A2v10.Data.Interfaces;
-using Newtonsoft.Json;
-using System;
 using System.IO;
 
-namespace A2v10.Data.Validator
+using Newtonsoft.Json;
+
+namespace A2v10.Data.Validator;
+
+public class JsonValidator
 {
-	public class JsonValidator
+	private readonly AllModels _models;
+
+	public static JsonValidator FromJson(String json)
 	{
-		private readonly AllModels _models;
+		return new JsonValidator(json);
+	}
 
-		public static JsonValidator FromJson(String json)
-		{
-			return new JsonValidator(json);
-		}
+	public static JsonValidator FromFile(String path)
+	{
+		var json = File.ReadAllText(path);
+		return new JsonValidator(json);
+	}
 
-		public static JsonValidator FromFile(String path)
-		{
-			var json = File.ReadAllText(path);
-			return new JsonValidator(json);
-		}
+	private JsonValidator(String json)
+	{
+		_models = JsonConvert.DeserializeObject<AllModels>(json)!;
+		_models.Parse();
+	}
 
-		private JsonValidator(String json)
-		{
-			_models = JsonConvert.DeserializeObject<AllModels>(json)!;
-			_models.Parse();
-		}
-
-		public IDataModelValidator CreateValidator(String name)
-		{
-			if (!_models.ContainsKey(name))
-				throw new DataValidationException($"Model {name} not found");
-			return _models[name];
-		}
+	public IDataModelValidator CreateValidator(String name)
+	{
+		if (!_models.ContainsKey(name))
+			throw new DataValidationException($"Model {name} not found");
+		return _models[name];
 	}
 }
