d43fcf5b1d1da497c2bac6fd248c2642f7e615b1
alex-kukhtin <alex.kukhtin@outlook.com>
2022-11-09 09:06:26 +0200

    publish nuget

--- publishnuget.cmd
+++ publishnuget.cmd
@@ -1,2 +1,14 @@
 dotnet build -c Release
+
+del /q ..\NuGet.local\*.*
+
+copy A2v10.Data.Interfaces\bin\Release\*.nupkg ..\NuGet.local
+copy A2v10.Data.Interfaces\bin\Release\*.snupkg ..\NuGet.local
+
+copy A2v10.Data.Providers\bin\Release\*.nupkg ..\NuGet.local
+copy A2v10.Data.Providers\bin\Release\*.snupkg ..\NuGet.local
+
+copy A2v10.Data\bin\Release\*.nupkg ..\NuGet.local
+copy A2v10.Data\bin\Release\*.snupkg ..\NuGet.local
+
 pause
