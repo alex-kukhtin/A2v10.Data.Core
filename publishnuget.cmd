f1fb621dbc8d8a418527d5cc686375f3d020ebe0
alex-kukhtin <alex.kukhtin@outlook.com>
2022-11-10 13:52:40 +0200

    publish.cmd fix

--- publishnuget.cmd
+++ publishnuget.cmd
@@ -1,3 +1,10 @@
+del /q A2v10.Data.Interfaces\bin\Release\*.nupkg
+del /q A2v10.Data.Interfaces\bin\Release\*.snupkg
+del /q A2v10.Data.Providers\bin\Release\*.nupkg
+del /q A2v10.Data.Providers\bin\Release\*.snupkg
+del /q A2v10.Data\bin\Release\*.nupkg
+del /q A2v10.Data\bin\Release\*.snupkg
+
 dotnet build -c Release
 
 del /q ..\NuGet.local\*.*
